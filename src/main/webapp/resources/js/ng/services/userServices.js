51d57c4bc4108cbfd159d3303f2296fcb8fcae60
ashmna <mnatsakanyan.ashot@gmail.com>
2015-06-14 23:31:31 +0400

    Added angular userServices

---
+++ src/main/webapp/resources/js/ng/services/userServices.js
@@ -0,0 +1,20 @@
+app.service('userServices', ['serverConnector', function(serverConnector) {
+    'use strict';
+
+
+    this.signIn = function(data) {
+        return serverConnector.send({
+            url : 'user/sign-in',
+            data: data
+        });
+    };
+
+    this.register = function(data) {
+        return serverConnector.send({
+            url : 'user/create',
+            data: data
+        });
+    };
+
+
+}]);
