32cd11e5609545352dfa55d07a52b87132ffadb1
Aram89 <aramkirakosyan89@gmail.com>
2015-07-10 16:04:14 -0700

    Changed registration paths.

--- src/main/java/org/proffart/babyroom/utils/RequestMappings.java
+++ src/main/java/org/proffart/babyroom/utils/RequestMappings.java
@@ -5,12 +5,12 @@ package org.proffart.babyroom.utils;
  */
 public class RequestMappings {
     public static final String user = "user";
-    public static final String CREATE_PARENT = "/create/parent";
-    public static final String CREATE_EXPERT = "/create/expert";
-    public static final String CREATE_SELLER = "/create/seller";
-    public static final String PARENT_LOGIN = "/login/parent";
-    public static final String EXPERT_LOGIN = "/login/expert";
-    public static final String SELLER_LOGIN = "/login/seller";
+    public static final String CREATE_PARENT = "/create-parent";
+    public static final String CREATE_EXPERT = "/create-expert";
+    public static final String CREATE_SELLER = "/create-seller";
+    public static final String PARENT_LOGIN = "/login-parent";
+    public static final String EXPERT_LOGIN = "/login-expert";
+    public static final String SELLER_LOGIN = "/login-seller";
 
     public static final String createPost = "/createPost";
     public static final String checkEmail = "/check-email";
