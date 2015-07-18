5ab18c353d46fb6431e13b9a1541db0c75026c79
Aram89 <aramkirakosyan89@gmail.com>
2015-07-17 22:09:25 -0700

    Added controller for file upload.

---
+++ web/src/main/java/org/proffart/babyroom/controller/FileController.java
@@ -0,0 +1,45 @@
+package org.proffart.babyroom.controller;
+
+import org.proffart.babyroom.utils.RequestMappings;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
+/**
+ * @author Aram Kirakosyan.
+ *
+ * Controller for file manipulations.
+ */
+@Controller
+public class FileController {
+
+
+
+    @RequestMapping(value= RequestMappings.UPLOAD_FILE, method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+                                                 @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + "!";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
+
+}
