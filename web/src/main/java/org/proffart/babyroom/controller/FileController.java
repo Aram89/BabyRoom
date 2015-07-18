ff14ab560ae125609c3b37dea324370e7e5f9b75
Aram89 <aramkirakosyan89@gmail.com>
2015-07-17 23:08:54 -0700

    Changes in file upload.

--- web/src/main/java/org/proffart/babyroom/controller/FileController.java
+++ web/src/main/java/org/proffart/babyroom/controller/FileController.java
@@ -1,5 +1,6 @@
 package org.proffart.babyroom.controller;
 
+import com.sun.xml.internal.ws.api.message.Attachment;
 import org.proffart.babyroom.utils.RequestMappings;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,11 +8,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 
+import javax.servlet.http.HttpServletResponse;
 import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Iterator;
 
 /**
  * @author Aram Kirakosyan.
@@ -24,22 +29,19 @@ public class FileController {
 
 
     @RequestMapping(value= RequestMappings.UPLOAD_FILE, method=RequestMethod.POST)
-    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
-                                                 @RequestParam("file") MultipartFile file){
-        if (!file.isEmpty()) {
-            try {
-                byte[] bytes = file.getBytes();
-                BufferedOutputStream stream =
-                        new BufferedOutputStream(new FileOutputStream(new File(name)));
-                stream.write(bytes);
+    public void UploadFile(MultipartHttpServletRequest request,HttpServletResponse response) throws IOException {
+            Iterator<String> itr=request.getFileNames();
+            MultipartFile file=request.getFile(itr.next());
+            String fileName=file.getOriginalFilename();
+            File dir = new File("/files/");
+            if (dir.isDirectory()){
+                File serverFile = new File(dir,fileName);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(file.getBytes());
                 stream.close();
-                return "You successfully uploaded " + name + "!";
-            } catch (Exception e) {
-                return "You failed to upload " + name + " => " + e.getMessage();
+            }else {
+                System.out.println("Error Found");
             }
-        } else {
-            return "You failed to upload " + name + " because the file was empty.";
         }
-    }
-
 }
