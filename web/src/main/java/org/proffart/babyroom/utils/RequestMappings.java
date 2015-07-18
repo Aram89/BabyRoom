8b0c478fe9713ce10e3c26b8edbaab31a5a33f7d
Aram89 <aramkirakosyan89@gmail.com>
2015-07-17 22:12:53 -0700

    Added path for file upload.

--- web/src/main/java/org/proffart/babyroom/utils/RequestMappings.java
+++ web/src/main/java/org/proffart/babyroom/utils/RequestMappings.java
@@ -4,6 +4,7 @@ package org.proffart.babyroom.utils;
  * @author Aram Kirakosyan.
  */
 public interface RequestMappings {
+    // Login and Registration.
     String USER = "user";
     String CREATE_PARENT = "/create-parent";
     String CREATE_EXPERT = "/create-expert";
@@ -12,11 +13,14 @@ public interface RequestMappings {
     String EXPERT_LOGIN = "/login-expert";
     String SELLER_LOGIN = "/login-seller";
     String CREATE_CHILDREN = "/create-children";
-
-    String CREATE_POST = "/create-post";
     String CHECK_EMAIL = "/check-email";
     String LOGIN = "/login";
 
+    // Posts
+    String CREATE_POST = "/create-post";
+    String UPLOAD_FILE = "/upload-file";
+
+    // Pages
     String LOGIN_PAGE = "/login";
     String CREATE_CHILDREN_PAGE = "/children-create";
     String INDEX = "/index";
