6f9d64295cf540b14e9b7b713c19b147e7905b47
VahanG <vahangevorgyan93@gmail.com>
2015-07-23 22:12:13 +0400

    file upload

--- web/src/main/java/org/proffart/babyroom/controller/FileController.java
+++ web/src/main/java/org/proffart/babyroom/controller/FileController.java
@@ -1,6 +1,8 @@
 package org.proffart.babyroom.controller;
 
 import org.proffart.babyroom.utils.RequestMappings;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,6 +18,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Iterator;
+import java.util.Random;
+
 import org.apache.commons.io.output.DeferredFileOutputStream;
 
 /**
@@ -29,13 +33,12 @@ public class FileController {
 
 
     @RequestMapping(value= RequestMappings.UPLOAD_FILE, method=RequestMethod.POST)
-    public void UploadFile( MultipartHttpServletRequest request,HttpServletResponse response) throws IOException {
+    public ResponseEntity UploadFile( MultipartHttpServletRequest request,HttpServletResponse response) throws IOException {
 
-        try{
             Iterator<String> itr=request.getFileNames();
             MultipartFile file=request.getFile(itr.next());
             String fileName = file.getOriginalFilename();
-            File dir = new File("/files/");
+            File dir = new File("C:\\magic");
             if (dir.isDirectory()){
                 File serverFile = new File(dir,fileName);
                 BufferedOutputStream stream = new BufferedOutputStream(
@@ -43,12 +46,11 @@ public class FileController {
                 stream.write(file.getBytes());
                 stream.close();
             }else {
+                dir.mkdir();
                 System.out.println("Error Found");
             }
-        } catch (Exception e) {
-            e.printStackTrace();
+        return new ResponseEntity(fileName, HttpStatus.OK);
         }
 
-        }
 
 }
--- web/src/main/webapp/resources/ng/ctrl/social.js
+++ web/src/main/webapp/resources/ng/ctrl/social.js
@@ -1,9 +1,60 @@
 /**
  * Created by pr on 07/03/2015.
  */
-app.controller('SocialController',function($scope,$http){
+app.controller('SocialController',function($scope,FileUploader,$http){
     $scope.theme='social-theme';
-    $scope.document = {};
+
+    var uploader = $scope.uploader = new FileUploader({
+        url: 'http://localhost:8080/upload-file'
+    });
+
+    uploader.filters.push({
+        name: 'customFilter',
+        fn: function(item /*{File|FileLikeObject}*/, options) {
+            return this.queue.length < 10;
+        }
+    });
+
+////----Callbacks----//
+    //uploader.onWhenAddingFileFailed = function(item /*{File|FileLikeObject}*/, filter, options) {
+    //    console.info('onWhenAddingFileFailed', item, filter, options);
+    //};
+    /*uploader.onAfterAddingFile = function(fileItem) {
+        console.info('onAfterAddingFile', fileItem);
+    };*/
+    /*uploader.onAfterAddingAll = function(addedFileItems) {
+        console.info('onAfterAddingAll', addedFileItems);
+    };*
+    /*uploader.onBeforeUploadItem = function(item) {
+        console.info('onBeforeUploadItem', item);
+    };*/
+    /*uploader.onProgressItem = function(fileItem, progress) {
+        console.info('onProgressItem', fileItem, progress);
+    };*/
+    /*uploader.onProgressAll = function(progress) {
+        console.info('onProgressAll', progress);
+    };*/
+/*    uploader.onSuccessItem = function(fileItem, response, status, headers) {
+        console.info('onSuccessItem', fileItem, response, status, headers);
+    };*/
+    uploader.onErrorItem = function(fileItem, response, status, headers) {
+        console.info('onErrorItem', fileItem, response, status, headers);
+    };
+    uploader.onCancelItem = function(fileItem, response, status, headers) {
+        console.info('onCancelItem', fileItem, response, status, headers);
+    };
+    uploader.onCompleteItem = function(fileItem, response, status, headers) {
+        console.info('onCompleteItem', response, status);
+    };
+    uploader.onCompleteAll = function(fileItem, response, status, headers) {
+        console.info('onCompleteItem', response, status);
+    };
+
+    //end callbacks---//
+
+
+
+/*    $scope.document = {};
     $scope.setTitle = function(fileInput) {
 
         var file=fileInput.value;
@@ -33,6 +84,6 @@ app.controller('SocialController',function($scope,$http){
             });
     };
 
-
+*/
 
 })
