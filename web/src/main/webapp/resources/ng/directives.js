55dd902ccec75b6550f9f2b97c05678256652f99
VahanG <vahangevorgyan93@gmail.com>
2015-08-30 23:11:44 +0400

    Changed saved file path.

--- web/src/main/webapp/resources/ng/directives.js
+++ web/src/main/webapp/resources/ng/directives.js
@@ -12,6 +12,12 @@ app.directive('timeLineItem', [function () {
 app.directive('post', ['fileUploadService', 'userServices', function (fileUploadService, userServices) {
     var ctrl = function ($scope) {
         var uploadsList = [];
+        $scope.events = [
+            "event1",
+            "event2",
+            "event3",
+            "event4"
+        ];
         $scope.post={
             status:'lkshdfk',
             postType:'',
@@ -25,19 +31,19 @@ app.directive('post', ['fileUploadService', 'userServices', function (fileUpload
                 uploadsList.splice(index,1)
             }
         };
+        //console.log($scope);
         function uploadFile() {
             /*var uploader = $scope.uploader=new FileUploader({
              url: 'http://localhost:8080/upload-file'
              });*/
             var uploader = $scope.uploader = fileUploadService.uploader();
-            console.log(uploader);
+            //console.log(uploader);
             uploader.filters.push({
                 name: 'imageFilter',
                 onWhenFailed: function () {
                     alert("Please select |jpg|png|jpeg|bmp|gif|mp4|")
                 },
                 fn: function (item /*{File|FileLikeObject}*/, options) {
-                    console.log('sds');
                     var type = '|' + item.type.slice(item.type.lastIndexOf('/') + 1) + '|';
                     return '|jpg|png|jpeg|bmp|gif|mp4|'.indexOf(type) !== -1;
                 }
@@ -50,15 +56,12 @@ app.directive('post', ['fileUploadService', 'userServices', function (fileUpload
                 //console.info('onCompleteItem', response, status);
                 fileItem.uID = response;
                 uploadsList.push(response);
-                console.log(fileItem);
             };
             uploader.onAfterAddingAll = function (addedFileItems) {
                 uploader.uploadAll();
-                //console.info('onAfterAddingAll', addedFileItems);
             };
             uploader.onCancelItem  = function (s, e, o) {
                 console.info(s);
-                console.log("dsf");
             }
         }
         uploadFile();
@@ -66,7 +69,6 @@ app.directive('post', ['fileUploadService', 'userServices', function (fileUpload
             userServices.createPost($scope.post)
                 .success(function(s){
                     console.log(s);
-                    console.log("sjhdjdhhj")
                 });
             console.log($scope.post);
         }
