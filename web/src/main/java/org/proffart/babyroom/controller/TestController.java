8ba2949c75635f4e21cfce0a45d5d420a663b688
Aram89 <aramkirakosyan89@gmail.com>
2015-09-14 23:37:06 -0700

    Added nodejs-connector module dependency and configs for publishing notifications to Redis.

--- web/src/main/java/org/proffart/babyroom/controller/TestController.java
+++ web/src/main/java/org/proffart/babyroom/controller/TestController.java
@@ -1,15 +1,31 @@
 package org.proffart.babyroom.controller;
 
+//import org.proffart.babyroom.nodejs.routes.RedisPublisher;
+import org.proffart.babyroom.data.Notification;
+import org.proffart.babyroom.redis.RedisPublisher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+
 @Controller
 public class TestController {
 
+    @Autowired
+    private RedisPublisher redisPublisher;
+
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String Test(@RequestParam(value = "page") String page) {
+        // Test notification.
+        Notification notification = new Notification();
+        notification.setReceiverId(2l);
+        notification.setSenderId(1l);
+        notification.setType("FRIEND_REQUEST");
+
+        // Testing redis.
+        redisPublisher.publish("test", notification);
         return page;
     }
 
